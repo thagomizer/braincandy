de3f1e785e1ade0e1bf3120957adff43d229ca5a
Aja Hammerly <aja.hammerly+github@gmail.com>
2015-09-03 21:07:20 -0700

    Bouncing!!!

--- bouncy-balls/bouncy_template.rb
+++ bouncy-balls/bouncy_template.rb
@@ -3,9 +3,38 @@
 require "graphics"
 
 class Ball < Graphics::Body
+  GRAVITY = [0, -2]
+
+  attr_accessor :color, :x, :y, :width, :window, :velocity
+
   def initialize w
     super
-    #TODO
+    @color    = :red
+    @x        = 50
+    @y        = 600
+    @width    = 15
+    @window   = w
+    @velocity = [0, 0]
+  end
+
+  def update
+    if self.on_edge? then
+      @velocity[1] *= -0.9
+    end
+
+    @velocity[0] += GRAVITY[0]
+    @velocity[1] += GRAVITY[1]
+
+    @x += @velocity[0]
+    @y += @velocity[1]
+
+    self.clip
+  end
+
+  def on_edge?
+    return true if @x < @width or @y < @width
+    return true if @x >= 640 - @width
+    return true if @y >= 640 - @width
   end
 
   def draw
@@ -16,15 +45,16 @@ end
 class BounceSimulation < Graphics::Simulation
   def initialize
     super 640, 640, 16, "Bounce"
-    #TODO
+    @ball = Ball.new self
   end
 
   def update n
-    #TODO
+    @ball.update
   end
 
   def draw n
-    #TODO
+    self.clear
+    self.circle @ball.x, @ball.y, @ball.width, @ball.color, true
   end
 end
 
